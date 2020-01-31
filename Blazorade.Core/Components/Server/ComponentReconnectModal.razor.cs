437e0d4e3a9825970e9aae96489771a2ddc7500e
Mika Berglund <mika.berglund@meteoriitti.com>
2020-01-31 15:17:42 +0200

    Initial commit of the ComponentReconnectModal component.

---
+++ Blazorade.Core/Components/Server/ComponentReconnectModal.razor.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blazorade.Core.Components.Server
+{
+    partial class ComponentReconnectModal
+    {
+        public ComponentReconnectModal()
+        {
+            this.BackdropBackgroundColor = "rgba(0, 0, 0, .75)";
+            this.ConnectionStateBackgroundColor = "rgba(255, 255, 255, .8)";
+            this.ConnectionStatePadding = "64px";
+        }
+
+
+        [Parameter]
+        public string BackdropBackgroundColor { get; set; }
+
+        [Parameter]
+        public string ConnectionStateBackgroundColor { get; set; }
+
+        [Parameter]
+        public string ConnectionStatePadding { get; set; }
+
+        [Parameter]
+        public RenderFragment ReconnectingTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment ReconnectFailedTemplate { get; set; }
+
+        [Parameter]
+        public RenderFragment ReconnectRejectedTemplate { get; set; }
+
+    }
+}
